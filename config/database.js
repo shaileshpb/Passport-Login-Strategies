70c53964e4cc2c767da176bcb21b89c86172a470
shailesh borkar <shailesh.borkar@sap.com>
2015-11-28 01:40:45 +0530

    Added MongoLabs

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
 
-    'url' : "mongodb://localhost/login"
+    'url' : "mongodb://heroku_zqkhs934:t4peugdoh8ssop5qvcf0a9nudt@ds059804.mongolab.com:59804/heroku_zqkhs934"
 };
